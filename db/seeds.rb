52973c62ded10adfef32889a9ea84eba66e9f18e
GesielRios <gesielrios@gmail.com>
2014-10-06 18:02:12 -0300

    Ajustes no seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(:name => "ENBATI ADMIN", 
+            :email => "enbati@gmail.com",
+            :password => "enbati2014",
+            :password_confirmation => "enbati2014")
