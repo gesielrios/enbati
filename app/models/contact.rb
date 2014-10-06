bea8ebae21fdbd6281e7828adad18d92816e423b
GesielRios <gesielrios@gmail.com>
2014-10-06 10:25:07 -0300

    Finalizacao da pagina principal do projeto.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ActiveRecord::Base
+  validates :email, :message, :name, :subject, presence: true
+  validates :email, format: { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/, message: "Digite um email válido." }
 end
